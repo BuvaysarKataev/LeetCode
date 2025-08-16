fe34511cb55d63b238f9a6913480a4601afa1ab7
Buvaysar <buvaysar2032@gmail.com>
2025-08-16 22:56:36 +0300

    feat: add findMaxConsecutiveOnes

---
+++ Algorithms/Easy/findMaxConsecutiveOnes.js
@@ -0,0 +1,18 @@
+function findMaxConsecutiveOnes(nums) {
+    let maxCount = 0;
+    let currentCount = 0;
+
+    for (let num of nums) {
+        if (num === 1) {
+            currentCount++;
+            maxCount = Math.max(maxCount, currentCount);
+        } else {
+            currentCount = 0;
+        }
+    }
+
+    return maxCount;
+}
+
+let nums = [1,1,0,1,1,1];
+console.log(findMaxConsecutiveOnes(nums));
